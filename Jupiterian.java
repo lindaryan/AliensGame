7df0133eca1983d7b9a7c8be402d975ef8832fb7
Linda Ryan <020080205@student.georgianc.on.ca>
2021-02-28 19:20:16 -0500

    add martian & jupiterian child classes

---
+++ Jupiterian.java
@@ -0,0 +1,16 @@
+package comp1069;
+
+public class Jupiterian extends Alien{
+    // Create a Jupiterian class that descends from Alien.
+    // Include a constructor that sets the Alien data fields with values you choose.
+    // For example, you can decide that a Martian has four eyes but a Jupiterian has only two.
+    public Jupiterian() {
+        super(3,3,3);
+    }
+
+    // Now add a draw() method. The draw() method accepts a Graphics object x- and y- starting coordinates.
+    // It draws the Aliens in any way you choose, using lines, ovals, rectangles, etc.
+
+    // Using the drawstring() method, include a description that names each drawing
+}
+
---
+++ Martian.java
@@ -0,0 +1,15 @@
+package comp1069;
+
+public class Martian extends Alien {
+    // Create a Martian class that descends from Alien.
+    // Include a constructor that sets the Alien data fields with values you choose.
+    // For example, you can decide that a Martian has four eyes but a Jupiterian has only two.
+    public Martian() {
+        super(1,4,3);
+    }
+
+    // Now add a draw() method. The draw() method accepts a Graphics object x- and y- starting coordinates.
+    // It draws the Aliens in any way you choose, using lines, ovals, rectangles, etc.
+
+    // Using the drawstring() method, include a description that names each drawing
+}
